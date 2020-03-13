213c75bc514f5758c676ef7d52ac5a09293b4503
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2020-03-13 18:09:53 -0300

    getVerbos é totalmente funiconal! Primeiro pega os verbos online e depois do arquivo `verbos_medievais.txt` e os coloca em `verbos.txt`

--- getVerbos.py
+++ getVerbos.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 # parte do código foi escrito por pythonprobr
 # o código original no arquivo converter.py
 
-ENTRADA = 'verbos1.txt'
-SAIDA = 'verbos2.txt'
+ENTRADA = 'verbos_medievais.txt'
+SAIDA = 'verbos.txt'
 
 
 def normalizar(txt):
@@ -21,36 +21,23 @@ def normalizar(txt):
 def chave(txt):
     """Manter palavras acentuadas após as não acentuadas"""
     codigos = tuple(ord(c) for c in txt)
-    return (normalizar(txt), codigos)
+    return (codigos, normalizar(txt))
 
 
 verbos = set()
-with open(ENTRADA, encoding='UTF-8') as entrada:
-    for i, linha in enumerate(entrada):
-        linha = linha.strip()
-        linha = linha.split('/')[0]
-        partes = linha.split('-')
-        if linha:  # para evitar palavra vazia
-            if len(partes) < 2:
-                verbos.add(linha)  # incluir palavra composta
-        if len(partes) > 1:
-            for palavra in partes:
-                if palavra:
-                    verbos.add(palavra)  # para evitar palavra vazia
 
-    qt_original = i
+# pegar verbos online
 
-msg = '{} palavras na lista original, {} na lista gerada: {} adicionadas'
-extra = len(verbos) - qt_original
-print(msg.format(qt_original, len(verbos), extra))
-
-# pergar verbos online
-
+# urls de todas as páginas contendo os verbos
 urls = set()
 for i in range(1, 51):
     urls.add('https://www.conjugacao.com.br/verbos-populares/' + str(i) + '/')
 
-i = 1
+print('Pegando os verbos online...')
+
+# pega os verbos de todas as 50 páginas
+# de conjugacao.c
+progressso = 2
 for url in urls:
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "lxml")
@@ -58,8 +45,30 @@ for url in urls:
     for page in pages:
         verb = page.get_text()
         verbos.add(verb)
-    print(str(i) + " pages")
-    i = i + 1
+    print('Progresso: ' + str(progressso) + '%')
+    progressso = progressso + 2
+
+qt_original = len(verbos)
+
+# pegar verbos do português medieval
+with open(ENTRADA, encoding='UTF-8') as entrada:
+    for i, linha in enumerate(entrada):
+        linha = linha.strip()
+        linha = linha.split('/')[0]
+        partes = linha.split('-')
+        if linha:  # para evitar palavra vazia
+            if len(partes) < 2:
+                verbos.add(linha.lower())  # incluir palavra composta
+        if len(partes) > 1:
+            for palavra in partes:
+                if palavra:
+                    verbos.add(palavra.lower())  # para evitar palavra vazia
+
+verbos = sorted(verbos, key=chave)
+
+msg = '{} palavras na lista original, {} na lista gerada: {} adicionadas'
+extra = len(verbos) - qt_original
+print(msg.format(qt_original, len(verbos), extra))
 
 verbos = sorted(verbos, key=chave)
 
